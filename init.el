9a4b448078226a1b5b9def5b414d0fbd10e3ff74
Luke Gaudreau <gaudreal@bc.edu>
2016-08-03 15:36:45 -0400

    Add Vagrant

--- init.el
+++ init.el
@@ -38,6 +38,7 @@ values."
      ;; spell-checking
      ;; syntax-checking
      ;; version-control
+     vagrant
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
